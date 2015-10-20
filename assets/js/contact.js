712a10a3ec465cddf0b02dfb9d8329b1e88ba883
Simon Gaeremynck <gaeremyncks@gmail.com>
2015-10-20 15:03:45 +0100

    Added contact and join forms

---
+++ assets/js/contact.js
@@ -0,0 +1,48 @@
+$(document).on('ready', function() {
+
+    $('form').on('submit', function() {
+        // Clear validation
+        $('.form-group').removeClass('has-error');
+
+        var name = $('input[name="name"]').val();
+        var email = $('input[name="email"]').val();
+        var message = $('textarea').val();
+
+        var isValid = true;
+
+        if (!name) {
+            $('input[name="name"]').parent().addClass('has-error');
+            isValid = false;
+        }
+        if (!email) {
+            $('input[name="email"]').parent().addClass('has-error');
+            isValid = false;
+        }
+        if (!message) {
+            $('textarea').parent().addClass('has-error');
+            isValid = false;
+        }
+
+        if (isValid) {
+            $.ajax({
+                'url': 'http://formspree.io/simon@fronteer.io',
+                'method': 'POST',
+                'data': {
+                    'name': name,
+                    'email': email,
+                    'message': message,
+                    '_subject': 'apereo.org contact form submission from ' + name
+                },
+                'dataType': 'json',
+                success: function(data) {
+                    alert('We have received your message and will be in touch shortly!');
+                },
+                error: function(err) {
+                    alert('Oh no, we couldn\'t send your message, you can always try sending an email to oae-contact@apereo.org');
+                }
+            });
+        }
+
+        return false;
+    });
+});
---
+++ assets/js/join.js
@@ -0,0 +1,51 @@
+$(document).on('ready', function() {
+
+    $('form').on('submit', function() {
+        // Clear validation
+        $('.form-group').removeClass('has-error');
+
+        var data = {};
+        var isValid = true;
+        var names = ['organisation', 'website', 'name', 'email', 'position', 'participation', 'questions'];
+        for (var i = 0; i < names.length; i++) {
+            var name = names[i];
+            data[name] = $('.form-group [name="' + name + '"]').val();
+            if (!data[name]) {
+                $('.form-group [name="' + name + '"]').parent().addClass('has-error');
+                isValid = false;
+            }
+        }
+
+        if (isValid) {
+            var message = [
+                'Organisation: ' + data['organisation'],
+                'Website: ' + data['website'],
+                'Name: ' + data['name'],
+                'Email: ' + data['email'],
+                'Position: ' + data['position'],
+                'Participation: ' + data['participation'],
+                'Questions/message:',
+                data['questions']
+            ].join('\n');
+            $.ajax({
+                'url': 'http://formspree.io/simon@fronteer.io',
+                'method': 'POST',
+                'data': {
+                    'name': data['name'],
+                    'email': data['email'],
+                    'message': message,
+                    '_subject': 'apereo.org join form submission from ' + data['name']
+                },
+                'dataType': 'json',
+                success: function(data) {
+                    alert('We have received your message and will be in touch shortly!');
+                },
+                error: function(err) {
+                    alert('Oh no, we couldn\'t send your message, you can always try sending an email to oae-contact@apereo.org');
+                }
+            });
+        }
+
+        return false;
+    });
+});
